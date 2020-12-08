7e32a6ee12fa321ad5db5cfa487ffe6a512bd9d5
Николаев Александр Игоревич <nikolaevai@ubrr.ru>
2020-12-08 14:45:52 +0500

    add login page and broken template

--- app/controller/pages.go
+++ app/controller/pages.go
@@ -1,14 +1,29 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
+	"path/filepath"
+	"text/template"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+//StartPage for users ...
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	text := "Приветствую тебя на стартовой странице этого сайта!"
-	//возвращаем простой текст
-	fmt.Fprint(rw, text)
+	//указываем путь к нужному файлу
+	path := filepath.Join("public", "html", "login.html")
+	common := filepath.Join("public", "html", "common.html")
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(path, common)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	//выводим шаблон клиенту в браузер
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 }
--- app/controller/users.go
+++ app/controller/users.go
@@ -1,23 +1,36 @@
 package controller
 
 import (
-	"encoding/json"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/iavealokin/MDSite/app/model"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+//GetUsers from JSON array to indexDynamic.html
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	//получаем список всех пользователей
-	users, err := model.GetAllUsers()
+	_, err := model.GetAllUsers()
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
-	//возвращаем список клиенту в формате JSON
-	err = json.NewEncoder(rw).Encode(users)
+	//указываем путь к файлу с шаблоном
+	main := filepath.Join("public", "html", "index.html")
+	common := filepath.Join("public", "html", "template.html")
+
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(main, common)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	//исполняем именованный шаблон "users", передавая туда массив со списком пользователей
+	err = tmpl.Execute(rw, nil)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return

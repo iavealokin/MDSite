8e916acdfc689a065803f36ebf0e7f4afb6b15f5
Николаев Александр Игоревич <nikolaevai@ubrr.ru>
2020-12-08 11:36:33 +0500

    add first files

---
+++ app/model/user.go
@@ -0,0 +1,24 @@
+package model
+
+//User struct ...
+type User struct {
+	Id      int
+	Name    string
+	Surname string
+}
+
+//GetAllUsers func ...
+func GetAllUsers() (users []User, err error) {
+	users = []User{
+		{1, "Джон", "До"},
+		{2, "Говард", "Рорк"},
+		{3, "Джек", "Доусон"},
+		{4, "Лизель", "Мемингер"},
+		{5, "Джейн", "Эйр"},
+		{6, "Мартин", "Иден"},
+		{7, "Джон", "Голт"},
+		{8, "Сэмвелл", "Тарли"},
+		{9, "Гермиона", "Грейнджер"},
+	}
+	return
+}
